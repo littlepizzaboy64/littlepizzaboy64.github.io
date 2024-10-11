05e64960ebbee73722f2c415a2d1c3808c1aee41
littlepizzaboy64 <stu.jonathan.cueva@ihsnola.org>
2024-10-11 18:30:15 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,7 +38,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+    createPlatform(0, 350, 350, 20);
+    createPlatform(1050, 350, 350, 20);
+    createPlatform(0, 605, 350, 20);
+    createPlatform(1050, 605, 350, 20);
+    createPlatform(500, 475, 375, 20);
 
 
     
@@ -47,7 +51,11 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+    createCollectable("database", 100, 200);
+    createCollectable("database", 1250, 200);
+    createCollectable("database", 670, 425,);
+    createCollectable("database", 100, 550);
+    createCollectable("database", 1250, 550);
 
 
     
@@ -56,10 +64,12 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("left", 100, 200);
+    createCannon("right", 750, 200);
+    createCannon("top", 750, 500);
 
 
 
-    
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
