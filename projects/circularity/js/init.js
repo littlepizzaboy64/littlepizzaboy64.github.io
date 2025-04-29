4d57f14dcd2e966f68e11415df7ccc89161e5b89
littlepizzaboy64 <stu.jonathan.cueva@ihsnola.org>
2025-04-29 20:16:12 +0000

    completed circularity project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,7 +32,7 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i < 640; i++) {
             drawCircle();
         }
 
